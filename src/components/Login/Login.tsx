b049113ff2938c4edc294a7a6cc1ef6f830dfe4a
kenzoikeizume <kenzoikeizume@hotmail.com>
2019-05-03 22:57:12 -0300

    Refector login with anonymosly

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,21 +1,23 @@
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import { withStyles, WithStyles } from '@material-ui/core/styles';
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 
-import * as React from "react";
-import { styles } from './utils';
+import { withStyles, WithStyles } from "@material-ui/core/styles";
 
+import {
+  FirebaseAuthConsumer,
+  FirebaseAuthProvider,
+} from "@react-firebase/auth";
 import * as firebase from "firebase/app";
 import "firebase/auth";
-import {
-  FirebaseAuthProvider,
-  FirebaseAuthConsumer
-} from "@react-firebase/auth";
-import { CONFIG } from '../../credentials';
+import * as React from "react";
+
+import { CONFIG } from "../../credentials";
+import { styles } from "./utils";
+import { AuthEmission } from "@react-firebase/auth/dist/types";
 
 interface IProps extends WithStyles<typeof styles> { }
 
@@ -41,54 +43,11 @@ class Login extends React.Component<IProps, {}> {
             </Typography>
             <div>
               <FirebaseAuthConsumer>
-                {({ isSignedIn, firebase }) => {
-                  if (isSignedIn === true) {
-                    return (
-                      <div>
-                        <h2>You're signed in 🎉 </h2>
-                        <Button
-                          variant="contained"
-                          onClick={() => {
-                            firebase
-                              .app()
-                              .auth()
-                              .signOut();
-                          }}
-                        >
-                          Sign out
-                        </Button>
-                      </div>
-                    );
-                  } else {
-                    return (
-                      <div>
-                        <h2>You're not signed in </h2>
-                        <Button
-                          variant="contained"
-                          onClick={() => {
-                            firebase
-                              .app()
-                              .auth()
-                              .signInAnonymously();
-                          }}
-                        >
-                          Sign in anonymously
-                        </Button>
-                      </div>
-                    );
-                  }
-                }}
+                {(authEmission) => this._RenderStateClient(authEmission)}
               </FirebaseAuthConsumer>
               <Button
                 variant="contained"
-                onClick={() => {
-                  try {
-                    const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-                    firebase.auth().signInWithPopup(googleAuthProvider);
-                  } catch (error) {
-                    console.log('error :', error);
-                  }
-                }}
+                onClick={this._GoogleSignIn}
               >
                 Login With Google
               </Button>
@@ -97,7 +56,52 @@ class Login extends React.Component<IProps, {}> {
         </main>
       </FirebaseAuthProvider>
     );
-  };
+  }
+
+  private _GoogleSignIn(): void {
+    try {
+      const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(googleAuthProvider);
+    } catch (error) {
+      console.log('error :', error);
+    }
+  }
+
+  private _RenderStateClient({ isSignedIn, firebase }: AuthEmission): JSX.Element {
+    if (isSignedIn === true) {
+      return (
+        <div>
+          <h2>You're signed in 🎉 </h2>
+          <Button
+            variant="contained"
+            onClick={this._SignOut}
+          >
+            Sign out
+          </Button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h2>You're not signed in </h2>
+          <Button
+            variant="contained"
+            onClick={this._AnonymouslySignIn}
+          >
+            Sign in anonymously
+          </Button>
+        </div>
+      );
+    }
+  }
+
+  private _AnonymouslySignIn(): void {
+    firebase.app().auth().signInAnonymously();
+  }
+
+  private _SignOut(): void {
+    firebase.app().auth().signOut();
+  }
 }
 
 export default withStyles(styles, { withTheme: true })(Login);
